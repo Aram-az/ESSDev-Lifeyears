d04aad5df739fee91eaa86da5cf2c92681dc508d
Wallace Su <wallacesu2015@gmail.com>
2025-11-10 01:12:29 -0500

    basic components + global styling
    - added navbar and footer with placeholder text/values
    - added global styling in index.css (change in future to colour palette, padding, margins, etc, chosen by team)
    - made landing page height more dynamic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          ESSDev Lifeyears
-        </h1>
-        <p className="text-gray-600">Welcome to the frontend!</p>
-      </div>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow bg-gray-100 min-h-[calc(100vh-200px)]">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">
+              ESSDev Lifeyears
+            </h1>
+            <p className="text-gray-600">Welcome to the frontend of the app!</p>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
